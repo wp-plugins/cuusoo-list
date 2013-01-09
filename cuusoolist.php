fa2bba364cdf8d68ec8228d578f5effd2edcb3c2
legendarydrew <legendarydrew@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-09 15:03:55 +0000

    Tagging version 1.3.1.
    git-svn-id: https://plugins.svn.wordpress.org/cuusoo-list/tags/1.3.1@650266 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cuusoolist.php
@@ -0,0 +1,568 @@
+<?php
+/*
+ Plugin Name: CUUSOO List
+ Description: Displays a list of specified LEGO&reg; CUUSOO projects in a widget.
+ Author: Drew Maughan
+ Version: 1.3
+ Author URI: http://perfectzerolabs.com
+*/
+
+class CUUSOOList
+{
+
+	const DOMAIN      = 'cuusoolist';
+
+	const METHOD_PAGE = 0;
+	const METHOD_API  = 1;
+
+	const EVENT_FETCH = 'cuusoolist_refresh';
+
+
+	/**
+	 * CUUSOOList::install()
+	 * Run when the plugin is activated.
+	 *
+	 * @return void
+	 */
+	function activate()
+	{
+		$list   = get_option('cuusoolist_projects', array());
+		$method = get_option('cuusoolist_method', CUUSOOList::METHOD_API);
+
+		add_option('cuusoolist_projects', $list);
+		add_option('cuusoolist_method', $method);
+		CUUSOOList::schedule_refresh();
+	}
+
+
+	/**
+	 * CUUSOOList::deactivate()
+	 * Runs when the plugin is deactivated.
+	 *
+	 * @return void
+	 */
+	function deactivate()
+	{
+		wp_clear_scheduled_hook( CUUSOOList::EVENT_FETCH );
+		// In the horrific event where multiple events are registered, this should remove all of them.
+	}
+
+	/**
+	 * CUUSOOList::uninstall()
+	 * Run when the plugin is removed.
+	 *
+	 * @return void
+	 */
+	function uninstall()
+	{
+		CUUSOOList::deactivate();
+		delete_option('cuusoolist_projects');
+		delete_option('cuusoolist_method');
+		delete_option('cuusoolist_fetched');
+	}
+
+
+	/**
+	 * CUUSOOList::admin_page()
+	 * Provides the HTML for the plugin's admin page.
+	 *
+	 * @return void
+	 */
+	function admin_page()
+	{
+		include('settings.php');
+	}
+
+
+	/**
+	 * CUUSOOList::add_options_menu()
+	 * Adds a menu item for the plugin's settings page (under the Settings menu).
+	 *
+	 * @return void
+	 */
+	function add_options_menu()
+	{
+		add_options_page('CUUSOO List', 'CUUSOO List', 1, 'cuusoolist.php', array('CUUSOOList', 'admin_page'));
+	}
+
+
+	/**
+	 * CUUSOOList::get_parent_url()
+	 * Returns the file name of the plugin file from the $_GET data.
+	 *
+	 * @return
+	 */
+	function get_parent_url()
+	{
+		if ( array_key_exists('page', $_GET) )
+		{
+			return $_GET['page'];
+		}
+	}
+
+
+	/**
+	 * CUUSOOList::handler()
+	 * Handles the addition and deletion of CUUSOO projects.
+	 *
+	 * @return void
+	 */
+	function handler()
+	{
+		load_plugin_textdomain( CUUSOOList::DOMAIN, dirname( plugin_basename( __FILE__ ) ) );
+
+		$title       = __('Definitions', CUUSOOList::DOMAIN);
+		$parent_file = 'options-general.php?page=' . CUUSOOList::get_parent_url();
+
+		if ( 'cuusoolist.php' == substr($parent_file, -14) )
+		{
+			$list = get_option('cuusoolist_projects');
+
+			//wp_reset_vars(array('action', 'definition'));
+
+			if ( isset($_GET['delete']) ) // Deleting more than one project
+			{
+				$action = 'delete-many';
+			}
+			else
+			{
+				$action = $_GET['action'];
+			}
+
+			switch ($action)
+			{
+				case 'add':
+
+					// Adding a CUUSOO project.
+
+					check_admin_referer('add_cuusoolist');
+
+					$id	     = intval( $_GET['id'] );
+					$label   = $_GET['label'];
+					$message = CUUSOOList::update($id, $label) ? 1 : 4;
+
+					wp_redirect("{$parent_file}&message={$message}");
+
+					exit;
+					break;
+
+				case 'update':
+
+					// Updating a CUUSOO project without fetching data.
+
+					check_admin_referer('update_cuusoolist');
+
+					$id	     = intval( $_GET['id'] );
+					$label   = $_GET['label'];
+					$message = CUUSOOList::update($id, $label, false) ? 3 : 5;
+
+					wp_redirect("{$parent_file}&message={$message}");
+
+					exit;
+					break;
+
+				case 'delete':
+
+					// Deleting (or removing) a CUUSOO project.
+
+					check_admin_referer('delete_cuusoolist');
+
+					if ( !current_user_can('manage_categories') )
+					{
+						wp_die( __('Nuh-uh!') );
+					}
+
+					$id      = $_GET['id'];
+					$message = 2;
+
+					CUUSOOList::delete($id);
+
+					wp_redirect( "{$parent_file}&message={$message}" );
+
+					exit;
+					break;
+
+				case 'delete-many':
+
+					// Deleting (removing) at least one CUUSOO project.
+
+					check_admin_referer('delete_cuusoolist');
+
+					if ( !current_user_can('manage_categories') )
+					{
+						wp_die( __('Nuh-uh!') );
+					}
+
+					$projects = $_GET['delete'];
+					foreach ( (array) $projects as $id )
+					{
+						CUUSOOList::delete($id);
+					}
+
+					// Display a slightly different message depending on how many (one or more) projects were removed.
+					$message = ( count($projects) > 1 ) ? 6 : 2;
+
+					wp_redirect("{$parent_file}&message={$message}");
+
+					exit;
+					break;
+
+				case 'method':
+
+					// Changing the data fetching method.
+
+					check_admin_referer('method_cuusoolist');
+
+					if ( !current_user_can('manage_categories') )
+					{
+						wp_die( __('Nuh-uh!') );
+					}
+
+					$method  = $_GET['which_method'];
+					$message = 7;
+					update_option('cuusoolist_method', $method);
+
+					wp_redirect("{$parent_file}&message={$message}");
+
+					exit;
+					break;
+
+			}
+		}
+	}
+
+
+	/**
+	 * CUUSOOList::show_list()
+	 * Displays a list of defined CUUSOO projects in a table.
+	 *
+	 * @return void
+	 */
+	function show_list($s, $n, $p = '1')
+	{
+		include('settings-list.php');
+	}
+
+
+	/**
+	 * CUUSOOList::show_add_form()
+	 * Displays a form for adding a CUUSOO project, or editing if the id variable is present in $_GET.
+	 *
+	 * @return void
+	 */
+	function show_add_form()
+	{
+		$project_id = array_key_exists('id', $_GET) ? $_GET['id'] : null;
+		include('settings-add.php');
+	}
+
+
+	/**
+	 * CUUSOOList::show_method_form()
+	 * Displays a form for changing the data fetching method.
+	 *
+	 * @return void
+	 */
+	function show_method_form()
+	{
+		include('settings-method.php');
+	}
+
+
+	/**
+	 * CUUSOOList::show_pagination()
+	 * Displays a pagination bar for the list of CUUSOO projects (in conjunction with CUUSOOList::show_list()).
+	 *
+	 * @return void
+	 */
+	function show_pagination($s, $n, $p, $t, $filter = false)
+	{
+		include('settings-pagination.php');
+	}
+
+
+	/**
+	 * CUUSOOList::update()
+	 * Updates (or adds) a project with the current project information.
+	 *
+	 * @return
+	 */
+	function update($id, $label, $fetch = true)
+	{
+		if ( empty($id) )
+		{
+			return false;
+		}
+		else
+		{
+			if ( $fetch )
+			{
+				CUUSOOList::refresh( $id, $label );
+			}
+			else
+			{
+				$list = get_option('cuusoolist_projects');
+				$list[$id]['label'] = esc_html( stripslashes($label) );
+				update_option('cuusoolist_projects', $list);
+			}
+			return true;
+		}
+	}
+
+
+	/**
+	 * CUUSOOList::delete()
+	 * Removes a CUUSOO project.
+	 *
+	 * @return
+	 */
+	function delete($project_id)
+	{
+		$list = get_option('cuusoolist_projects');
+		if ( array_key_exists($project_id, $list) )
+		{
+			unset($list[$project_id]);
+			update_option('cuusoolist_projects', $list);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+	/**
+	 * CUUSOOList::count_projects()
+	 * Returns the number of defined CUUSOO projects.
+	 *
+	 * @return
+	 */
+	function count_projects()
+	{
+		$list = get_option('cuusoolist_projects');
+		return count($list);
+	}
+
+
+	/**
+	 * CUUSOOList::refresh()
+	 * "Refreshes" a CUUSOO project with the current project information.
+	 * This is reluctantly done by "page scraping" as the CUUSOO API doesn't provide the number of page views (required
+	 * to calculate the supporters/viewers ratio). This probably results in incrementing views by one every time.
+	 *
+	 * @return
+	 */
+	function refresh($project_id, $label)
+	{
+		$project_id = intval($project_id);
+		if ( $project_id == 0 ) return;
+
+		$list   = get_option('cuusoolist_projects');
+		$method = get_option('cuusoolist_method');
+
+		// Perform some page scraping, which is not ideal because it may trigger a page view - but is necessary to get
+		// information that the API doesn't provide (title and views).
+		try
+		{
+			$values = array();
+
+			switch ( $method )
+			{
+				case CUUSOOList::METHOD_API:
+
+					// Fetching data via the API.
+
+					$url  = "http://lego.cuusoo.com/api/participations/get/{$project_id}.json";
+					$json = file_get_contents($url);
+					$data = json_decode($json);
+
+					$values = array(
+						'supports'  => $data->participants->supporters,
+						'bookmarks' => $data->participants->bookmarks
+					);
+
+					break;
+
+				case CUUSOOList::METHOD_PAGE:
+
+					// Fetching data via page scraping (ugh).
+
+					$url  = "http://lego.cuusoo.com/ideas/view/{$project_id}";
+					$page = file_get_contents($url);
+
+					// Main image (to get the URL of the thumbnail).
+					preg_match('/src="(.*)thumb640x360.jpg" alt="Idea Image"\/>/i', $page, $image);
+
+					// Project title.
+					preg_match('/<div id="ideaName" class="projectTitle"><p>(.*)<\/p><\/div>/i', $page, $title);
+
+					// Number of supporters.
+					preg_match('/<ul class="supporters">(.*) supporters<\/ul>/i', $page, $support);
+
+					// Number of views.
+					preg_match('/<ul class="views">(.*) views<\/ul>/i', $page, $view);
+
+					// Number of bookmarks.
+					preg_match('/<ul class="followers">(.*) bookmarked<\/ul>/i', $page, $bookmark);
+
+					// Just to be sure, remove any commas from the supports and views counts.
+					$supports_num = intval(str_replace(',', '', $support[1]));
+					$views_num    = intval(str_replace(',', '', $view[1]));
+
+					// Get the juice!
+					$values = array(
+						'title'      => $title[1],
+						'thumbnail'  => $image[1] . 'thumb81x55.jpg',
+						'supports'   => $support[1],
+						'views'      => $view[1],
+						'bookmarks'  => $bookmark[1],
+						'ratio'      => round(($supports_num / $views_num) * 100), // ratio of supports/views.
+					);
+
+					break;
+			}
+
+			// If we're updating an existing project, calculate the difference in supporters between now and the last
+			// update.
+			if ( array_key_exists($project_id, $list) )
+			{
+				$last_supports  = intval(str_replace(',', '', $list[$project_id]['supports']));
+				$values['diff'] = $values['supports'] - $last_supports;
+			}
+			else
+			{
+				$values['diff'] = '--';
+			}
+
+			// Don't forget to add the label!
+			$values['label'] = $label;
+
+			// Replace the existing data for the project.
+			$list[$project_id] = $values;
+
+		}
+		catch (Exception $e)
+		{
+			// If for some reason there was a problem with updating a project.
+?>
+		<div id="message" class="updated fade">
+			<p><?php echo "Could not fetch project information for {$project_id}: " . $e->getMessage() ?></p>
+		</div>
+<?php
+		}
+
+		// Update the list of CUUSOO projects.
+		update_option('cuusoolist_projects', $list);
+	}
+
+
+	/**
+	 * CUUSOOList::refresh_projects()
+	 * Refreshes each defined CUUSOO project with current information.
+	 *
+	 * @return void
+	 */
+	function refresh_projects()
+	{
+		$list = get_option('cuusoolist_projects');
+		foreach ( $list as $id => $values )
+		{
+			CUUSOOList::refresh( $id );
+		}
+
+		// Set a 'last updated' date.
+		update_option('cuusoolist_fetched', current_time('timestamp'));
+	}
+
+
+   	/**
+   	 * CUUSOOList::register_widget()
+   	 * Registers the sidebar widget for inclusion in themes.
+   	 *
+   	 * @return void
+   	 */
+   	function register_widget()
+   	{
+   		register_widget( 'CUUSOOList_Widget' );
+	}
+
+
+
+	/**
+	 * CUUSOOList::register_dashboard_widget()
+	 * Registers a widget to be displayed in the WordPress dashboard.
+	 *
+	 * @return void
+	 */
+	function register_dashboard_widget()
+	{
+		global $wp_meta_boxes;
+		wp_add_dashboard_widget( 'CUUSOOList_Dashboard_Widget', 'CUUSOO List', array('CUUSOOList', 'dashboard_widget') );
+	}
+
+
+	/**
+	 * CUUSOOList::dashboard_widget()
+	 * Outputs the contents of the dashboard widget.
+	 *
+	 * @return void
+	 */
+	function dashboard_widget()
+	{
+		include 'widget-dashboard.php';
+	}
+
+
+	/**
+	 * CUUSOOList::schedule_refresh()
+	 * Sets up an event for refreshing CUUSOO project data, if one hasn't been set up.
+	 * Refreshing of data occurs once a day.
+	 *
+	 * @return void
+	 */
+	function schedule_refresh()
+	{
+		if ( CUUSOOList::next_fetch() === false )
+		{
+			wp_schedule_event( time(), 'daily', CUUSOOList::EVENT_FETCH );
+		}
+	}
+
+
+	/**
+	 * CUUSOOList::next_fetch()
+	 * Returns the timestamp of the next fetch event.
+	 *
+	 * @return int
+	 */
+	function next_fetch()
+	{
+		return wp_next_scheduled( CUUSOOList::EVENT_FETCH );
+	}
+
+
+}
+
+// Initialise the plugin, adding hooks for installation and uninstallation.
+add_action( 'admin_init', array('CUUSOOList', 'handler') );
+register_activation_hook( __FILE__, array( 'CUUSOOList', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'CUUSOOList', 'deactivate' ) );
+register_uninstall_hook( __FILE__, array( 'CUUSOOList', 'uninstall' ) );
+
+// Check whether a fetching event is scheduled.
+add_action('wp', array('CUUSOOList', 'schedule_refresh'));
+
+// Display a version of the widget for the admin-side dashboard.
+add_action( 'wp_dashboard_setup', array('CUUSOOList', 'register_dashboard_widget') );
+
+// Set up an action for refreshing the data for each project.
+add_action( CUUSOOList::EVENT_FETCH, array('CUUSOOList', 'refresh_projects') );
+
+// Add a menu item for the plugin.
+add_action( 'admin_menu', array('CUUSOOList', 'add_options_menu') );
+
+
+// Include the widget class and initialise it so it can be added to a theme.
+include('widget.php');
+add_action( 'widgets_init', array( 'CUUSOOList', 'register_widget' ) );
---
+++ readme.txt
@@ -0,0 +1,91 @@
+=== CUUSOO List ===
+Contributors: legendarydrew
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=89QB8KQSAQ3RE
+Tags: cuusoo, lego, list, widget
+Requires at least: 3.5
+Tested up to: 3.5
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Maintains a list of LEGO CUUSOO projects to display in a widget.
+
+== Description ==
+
+This plugin will allow the user to maintain a list of specific projects on the LEGO CUUSOO web site, and display
+them on their WordPress site using a widget. Data for each specified project is obtained from the LEGO CUUSOO web
+site and made available for display. The widget's template can be customised to get the layout you want.
+
+An dashboard widget that displays the list of projects, along with any changes in the number of supporters, is also
+added. This will be displayed by default, but can be turned off via the 'Screen Options' menu.
+
+I developed this plugin for [SilentMode.tv](http://silentmode.tv) to feature my own CUUSOO projects:
+
+* CATAWOL Records modular building http://lego.cuusoo.com/ideas/view/1760
+* Graduates and Gorillas: the game http://lego.cuusoo.com/ideas/view/15356
+* CATAWOL Records Studio One http://lego.cuusoo.com/ideas/view/20284
+
+If you have any suggestions for improvements to this plugin, feel free to contact me through
+http://silentmode.tv/contact/.
+
+== Installation ==
+
+1. Extract and copy the 'cuusoo-list' folder to the '/wp-content/plugins/' directory.
+1. Activate the 'CUUSOO List' plugin through the 'Plugins' menu in WordPress.
+1. Add the LEGO CUUSOO projects you want to track via the 'Settings > CUUSOO List' menu.
+1. Add the 'CUUSOO List' widget to a widget placeholder via the 'Appearance > Widgets' menu.
+
+The default template for the widget is 'widget-cuusoolist.php' in the plugin folder, which displays a basic unordered
+list. This can be overridden by creating a 'widget-cuusoolist.php' template file in your theme folder.
+
+== Data fetching ==
+
+Data for each project is fetched as soon as a project is added, and then once a day afterward.
+
+There are two methods available for fetching project data from the LEGO CUUSOO web site:
+
+* **API**: this fetches JSON data for the project. This is the fastest and least intrusive method, however only the
+number of supporters and bookmarks for the project is made available. You can use the label field to give each project a
+title.
+* **Page scrape**: this extracts project data from the project's page. While more project data (title, thumbnail and
+number of views) is made available, this method is likely to generate unwanted page views. If the extra data isn't
+necessary, please stick with the API method.
+
+The data fetching method can be set via the 'Settings > CUUSOO List' menu. Any changes will take effect on the next
+fetch.
+
+
+== Screenshots ==
+
+1. This is the default interface: the API data fetching method is selected by default.
+2. This is the interface when the page scrape method is selected: not the presence of extra columns in the list.
+3. A demonstration of the default CUUSOO List widget.
+4. An example of a custom CUUSOO List widget template, as used on [SilentMode.tv](http://silentmode.tv).
+
+== Frequently Asked Questions ==
+
+None yet...
+
+== Upgrade Notice ==
+
+None yet...
+
+== Changelog ==
+
+= 1.3.1 =
+* Better plugin deactivation handling: only the events for fetching project data should be removed. Project data now
+isn't removed unless the plugin is deleted (via the Plugins menu).
+
+= 1.3 =
+* Fixed a major issue with the fetching event, where more than one could be in effect at once. An absolute nightmare if
+you happen to be page scraping.
+* Labels were not being saved.
+* Corrected messages!
+
+= 1.2 =
+* First publicly available version.
+
+== Disclaimer ==
+
+LEGO is trademark of The LEGO Group. CUUSOO is a trademark of CUUSOO SYSTEM Co., Ltd. and Elephant Design Co. Ltd.
+None of these companies has anything to do with this plugin.
